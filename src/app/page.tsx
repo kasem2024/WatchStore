37416d3a51f47416459cd06cbad89b7ab699be60
KaSeM5 <kasem200mohareb@gmail.com>
2024-07-11 13:22:26 +0300

    adding url to env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
   const {data:products , isLoading , refetch} = useQuery({
     queryKey:['products'],
     queryFn:async()=>{
-      const  {data} = await axios.post<QueryResult<TProduct>[]>('http://localhost:3000/api/products',{
+      const  {data} = await axios.post<QueryResult<TProduct>[]>(`${process.env.URL}`,{
         filter:{
           sort:filter.sort,
           color:filter.color,
